96bae98e5d95e4448c9a6c40f7b24774322ace39
kivattt <kivattt@gmail.com>
2024-12-13 02:06:30 +0100

    Compile without debug flag

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-#g++ -std=c++23 -O2 -c main.cpp -o whodunnit.o && g++ -std=c++23 -O2 whodunnit.o -o whodunnit -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
+g++ -std=c++23 -O2 -c main.cpp -o whodunnit.o && g++ -std=c++23 -O2 whodunnit.o -o whodunnit -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
 
 # -g debug flag
-g++ -std=c++23 -O2 -g -c main.cpp -o whodunnit.o && g++ -std=c++23 -O2 -g whodunnit.o -o whodunnit -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
+#g++ -std=c++23 -O2 -g -c main.cpp -o whodunnit.o && g++ -std=c++23 -O2 -g whodunnit.o -o whodunnit -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
